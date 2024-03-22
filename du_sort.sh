2abaec7a8c24b92873ddeb31d7a747be75e9f5df
Marco Merlin <marcomerli+github@gmail.com>
2024-03-22 10:36:49 +0100

    Update du_sort.sh
    remove -S option, add max-depth

--- du_sort.sh
+++ du_sort.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # get disk usage and list 10 top entries
 # https://www.tecmint.com/find-top-large-directories-and-files-sizes-in-linux/
-du -Sh | sort -rh | head -10
+du -h --max-depth=1 | sort -rh | head -20
